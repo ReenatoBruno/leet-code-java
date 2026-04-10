1be2c669baca3f69020b3b33075fd8bd073cc430
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-04-10 15:44:59 -0300

    feat: setup TwoSum class and initial boilerplate

---
+++ src/com/leetcode/easy/TwoSum.java
@@ -0,0 +1,12 @@
+package com.leetcode.easy;
+
+public class TwoSum {
+
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+    }
+}
