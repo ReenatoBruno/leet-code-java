461f1673be2bf0e7cdd76f24f9e256755032013f
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-04-13 18:19:42 -0300

    feat: solve Reverse Linked List problem

--- src/com/leetcode/easy/ReverseLinkedList.java
+++ src/com/leetcode/easy/ReverseLinkedList.java
@@ -1,8 +1,31 @@
 package com.leetcode.easy;
 
 public class ReverseLinkedList {
+    public ListNode solveReverseList(ListNode head) {
+
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextTemp =  current.next;
+            current.next = prev;
+            prev = current;
+            current = nextTemp;
+        }
+        return prev;
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + (node.next != null ? " -> " : ""));
+            node = node.next;
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args) {
+
+        ReverseLinkedList solution = new ReverseLinkedList();
 
         ListNode head =  new ListNode(1);
         head.next = new ListNode(2);
@@ -10,5 +33,10 @@ public class ReverseLinkedList {
         head.next.next.next = new ListNode(4);
         head.next.next.next.next = new ListNode(5);
 
+        System.out.println("Original list" + head);
+
+        ListNode reverted = solution.solveReverseList(head);
+        System.out.println("Reversed list: ");
+        printList(reverted);
     }
 }
